0f0049321d3e153b64b20fd17c69135de9277b9d
LSaldyt <lucassaldyt@gmail.com>
2023-03-25 23:06:23 -0700

    Updated!

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,25 @@ use hw::port::mode::Input;
 use hw::port::PinOps;
 use hw::port::mode::Io;
 
-struct Axis<P,D,E> {
+struct Axis<P,D> {
     pulse  : Pin<Output,P>,
     dir    : Pin<Output,D>,
-    enable : Option<Pin<Output,E>>,
     delay  : u32
 }
 
-fn make_axis<I,P,D,E>(pulse  : Pin<I, P>, 
+fn make_axis<I,P,D>(pulse  : Pin<I, P>, 
                       dir    : Pin<I, D>, 
-                      enable : Option<Pin<I, E>>, 
                       delay  : u32
-             ) -> Axis<P,D,E> where I: Io, P: PinOps, D: PinOps, E: PinOps 
+             ) -> Axis<P,D> where I: Io, P: PinOps, D: PinOps
 { 
-    Axis { pulse  : pulse .into_output()), 
-           dir    : dir   .into_output()), 
-           enable : match enable {
-               None    => None,
-               Some(e) => Some(e.into_output()))
-           },
+    Axis { pulse  : pulse .into_output(), 
+           dir    : dir   .into_output(), 
            delay  : delay
     }
 }
 
-impl<P: PinOps, D: PinOps, E: PinOps> Axis<P,D,E> {
+impl<P: PinOps, D: PinOps> Axis<P,D> {
     fn turn (&mut self, steps : u32, direction : bool) {
-        match self.enable.as_mut() {
-            None        => (),
-            Some(mut e) => e.set_low()
-        }
         if direction {
             self.dir.set_high();
         } else {
@@ -50,13 +40,11 @@ impl<P: PinOps, D: PinOps, E: PinOps> Axis<P,D,E> {
             self.pulse.set_low();
             hw::delay_us(self.delay);
         }
-        match self.enable.as_mut() {
-            None        => (),
-            Some(mut e) => e.set_high()
-        }
         true;
     }
 }
+
+
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -65,17 +53,44 @@ fn main() -> ! {
 
     let mut led = pins.d13.into_output();
 
-    // let mut ax1 = make_axis(pins.d43, pins.d41, None, 4000);
-    // let mut ax2 = make_axis(pins.d39, pins.d37, None, 4000);
-    // let mut ax3 = make_axis(pins.d47, pins.d45, None, 4000);
-    // let mut ax4 = make_axis(pins.a6,  pins.a7,  Some(pins.a2),  600);
-    // let mut ax5 = make_axis(pins.a0,  pins.a1,  Some(pins.d38), 1000);
-     let mut ax6 = make_axis(pins.d46, pins.d48, Some(pins.a8),  200);
+    let mut main = pins.d32.into_output();
+
+    let mut enable_6 = pins.a8.into_output();
+    let mut enable_5 = pins.d38.into_output();
+    let mut enable_4 = pins.a2.into_output();
+    enable_6.set_low();
+    enable_5.set_low();
+    enable_4.set_low();
+
+    // Pulse, direction, delay
+    let mut ax1 = make_axis(pins.d43, pins.d41, 4000);
+    let mut ax2 = make_axis(pins.d39, pins.d37, 4000);
+    let mut ax3 = make_axis(pins.d47, pins.d45, 4000);
+    let mut ax4 = make_axis(pins.a6,  pins.a7,  200);
+    let mut ax5 = make_axis(pins.a0,  pins.a1,  200);
+    let mut ax6 = make_axis(pins.d46, pins.d48, 200);
 
     led.set_high();
-    ax1.turn(10000, true);
-    ax6.turn(6000, false);
+    main.set_low();
+    // ax1.turn(1000, true);
+    // ax1.turn(1000, true);
+    // ax1.turn(1000, false);
+    // ax2.turn(1000, true);
+    // ax2.turn(1000, false);
+    ax3.turn(1000, true);
+    ax3.turn(1000, false);
+    // ax4.turn(6000, true);
+    // ax4.turn(6000, false);
+    // ax5.turn(6000, true);
+    // ax5.turn(6000, false);
+    // ax6.turn(6000, true);
+    // ax6.turn(6000, false);
     led.set_low();
+
+    main.set_high();
+    enable_4.set_high();
+    enable_5.set_high();
+    enable_6.set_high();
 
     loop {
         led.toggle();
