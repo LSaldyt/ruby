25c3a324b2e723cf980b4509e169e06d8f00da8f
LSaldyt <lucassaldyt@gmail.com>
2023-03-29 10:08:46 -0700

    All-axes demo

--- src/main.rs
+++ src/main.rs
@@ -58,41 +58,44 @@ fn main() -> ! {
     let mut enable_6 = pins.a8.into_output();
     let mut enable_5 = pins.d38.into_output();
     let mut enable_4 = pins.a2.into_output();
-    enable_6.set_low();
-    enable_5.set_low();
-    enable_4.set_low();
+    enable_4.set_high();
+    enable_5.set_high();
+    enable_6.set_high();
 
     // Pulse, direction, delay
-    let mut ax1 = make_axis(pins.d43, pins.d41, 10000);
-    let mut ax2 = make_axis(pins.d39, pins.d37, 10000);
-    let mut ax3 = make_axis(pins.d47, pins.d45, 10000);
+    let mut ax1 = make_axis(pins.d43, pins.d41, 4000);
+    let mut ax2 = make_axis(pins.d39, pins.d37, 4000);
+    let mut ax3 = make_axis(pins.d47, pins.d45, 4000);
     let mut ax4 = make_axis(pins.a6,  pins.a7,  200);
     let mut ax5 = make_axis(pins.a0,  pins.a1,  200);
     let mut ax6 = make_axis(pins.d46, pins.d48, 200);
 
-    led.set_high();
     main.set_low();
-    main.set_high();
-    //ax1.turn(1000, true);
-    //ax1.turn(1000, false);
-    // ax2.turn(1000, true);
-    // ax2.turn(1000, false);
-    // ax3.turn(1000, true);
-    // ax3.turn(1000, false);
 
-    main.set_high();
+    led.set_high();
+    ax1.turn(500, true);
+    ax1.turn(500, false);
+    ax2.turn(1000, false);
+    ax2.turn(1000, true);
+    ax3.turn(1000, false);
+    ax3.turn(1000, true);
 
-    // ax4.turn(6000, true);
-    // ax4.turn(6000, false);
-    // ax5.turn(6000, true);
-    // ax5.turn(6000, false);
-    // ax6.turn(6000, true);
-    // ax6.turn(6000, false);
+    enable_6.set_low();
+    enable_5.set_low();
+    enable_4.set_low();
+
+    ax4.turn(6000, true);
+    ax4.turn(6000, false);
+    ax5.turn(6000, true);
+    ax5.turn(6000, false);
+    ax6.turn(6000, true);
+    ax6.turn(6000, false);
     led.set_low();
 
     enable_4.set_high();
     enable_5.set_high();
     enable_6.set_high();
+    main.set_high();
 
     loop {
         led.toggle();
