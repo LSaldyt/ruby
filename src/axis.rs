02c9adf4bc530959fd3db74db41353b86acbfcb1
LSaldyt <lucassaldyt@gmail.com>
2023-03-30 09:07:02 -0700

    WIP

--- src/axis.rs
+++ src/axis.rs
@@ -3,31 +3,40 @@ pub mod axis {
     use panic_halt as _;
     use arduino_hal as hw;
 
+    use hw::port::Pins;
     use hw::port::Pin;
     use hw::port::mode::Output;
     // use hw::port::mode::Input; // I template param
     use hw::port::PinOps;
     use hw::port::mode::Io;
+    use hw::port::PB7;
+    use hw::port::PC5;
+    use hw::port::PF2;
+    use hw::port::PD7;
+    use hw::port::PK0;
 
     pub struct Axis<P,D> {
         pulse  : Pin<Output,P>,
         dir    : Pin<Output,D>,
+        large  : bool,
         delay  : u32
     }
 
     pub fn make_axis<I,P,D>(pulse  : Pin<I, P>, 
                         dir    : Pin<I, D>, 
+                        large  : bool,
                         delay  : u32
                  ) -> Axis<P,D> where I: Io, P: PinOps, D: PinOps
     { 
         Axis { pulse  : pulse .into_output(), 
                dir    : dir   .into_output(), 
+               large  : large,
                delay  : delay
         }
     }
 
     impl<P: PinOps, D: PinOps> Axis<P,D> {
-        pub fn turn (&mut self, steps : u32, direction : bool) {
+        pub fn step (&mut self, steps : u32, direction : bool) {
             if direction {
                 self.dir.set_high();
             } else {
@@ -40,6 +49,24 @@ pub mod axis {
                 hw::delay_us(self.delay);
             }
             true;
+        }
+    }
+
+    pub struct Enable<P1,P2,P3,P4,P5> {
+        led  : Pin<Output, P1>,
+        main : Pin<Output, P2>,
+        en4  : Pin<Output, P3>,
+        en5  : Pin<Output, P4>,
+        en6  : Pin<Output, P5>
+    }
+
+    pub fn make_enable (pins : Pins) -> Enable <PB7, PC5, PF2, PD7, PK0>{
+        Enable {
+            led  : pins.d13.into_output(),
+            main : pins.d32.into_output(),
+            en4  : pins.a2.into_output(),
+            en5  : pins.d38.into_output(),
+            en6  : pins.a8.into_output()
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 mod axis;
 use axis::axis::make_axis;
+use axis::axis::make_enable;
 
 use panic_halt as _;
 use arduino_hal as hw;
@@ -12,10 +13,10 @@ fn main() -> ! {
     let peripherals = hw::Peripherals::take().unwrap();
     let pins        = hw::pins!(peripherals);
 
+    let mut enable = make_enable(pins);
+
     let mut led = pins.d13.into_output();
-
     let mut main = pins.d32.into_output();
-
     let mut enable_6 = pins.a8.into_output();
     let mut enable_5 = pins.d38.into_output();
     let mut enable_4 = pins.a2.into_output();
@@ -24,33 +25,33 @@ fn main() -> ! {
     enable_6.set_high();
 
     // Pulse, direction, delay
-    let mut ax1 = make_axis(pins.d43, pins.d41, 4000);
-    let mut ax2 = make_axis(pins.d39, pins.d37, 4000);
-    let mut ax3 = make_axis(pins.d47, pins.d45, 4000);
-    let mut ax4 = make_axis(pins.a6,  pins.a7,  200);
-    let mut ax5 = make_axis(pins.a0,  pins.a1,  200);
-    let mut ax6 = make_axis(pins.d46, pins.d48, 200);
+    let mut ax1 = make_axis(pins.d43, pins.d41, true, 4000);
+    let mut ax2 = make_axis(pins.d39, pins.d37, true, 4000);
+    let mut ax3 = make_axis(pins.d47, pins.d45, true, 4000);
+    let mut ax4 = make_axis(pins.a6,  pins.a7,  false, 200);
+    let mut ax5 = make_axis(pins.a0,  pins.a1,  false, 200);
+    let mut ax6 = make_axis(pins.d46, pins.d48, false, 200);
 
     main.set_low();
 
     led.set_high();
-    ax1.turn(500, true);
-    ax1.turn(500, false);
-    ax2.turn(1000, false);
-    ax2.turn(1000, true);
-    ax3.turn(1000, false);
-    ax3.turn(1000, true);
+    ax1.step(500, true);
+    ax1.step(500, false);
+    ax2.step(1000, false);
+    ax2.step(1000, true);
+    ax3.step(1000, false);
+    ax3.step(1000, true);
 
     enable_6.set_low();
     enable_5.set_low();
     enable_4.set_low();
 
-    ax4.turn(6000, true);
-    ax4.turn(6000, false);
-    ax5.turn(6000, true);
-    ax5.turn(6000, false);
-    ax6.turn(6000, true);
-    ax6.turn(6000, false);
+    ax4.step(6000, true);
+    ax4.step(6000, false);
+    ax5.step(6000, true);
+    ax5.step(6000, false);
+    ax6.step(6000, true);
+    ax6.step(6000, false);
     led.set_low();
 
     enable_4.set_high();
