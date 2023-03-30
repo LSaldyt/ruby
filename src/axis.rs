70dc5c54ba1b974d66489f9901d7c2a54366342a
LSaldyt <lucassaldyt@gmail.com>
2023-03-30 09:29:22 -0700

    Refactor to hide/abstract enable pins

--- src/axis.rs
+++ src/axis.rs
@@ -3,17 +3,10 @@ pub mod axis {
     use panic_halt as _;
     use arduino_hal as hw;
 
-    use hw::port::Pins;
     use hw::port::Pin;
     use hw::port::mode::Output;
-    // use hw::port::mode::Input; // I template param
     use hw::port::PinOps;
     use hw::port::mode::Io;
-    use hw::port::PB7;
-    use hw::port::PC5;
-    use hw::port::PF2;
-    use hw::port::PD7;
-    use hw::port::PK0;
 
     pub struct Axis<P,D> {
         pulse  : Pin<Output,P>,
@@ -36,38 +29,44 @@ pub mod axis {
     }
 
     impl<P: PinOps, D: PinOps> Axis<P,D> {
-        pub fn step (&mut self, steps : u32, direction : bool) {
+        pub fn step<P1,P2,P3,P4,P5> (&mut self, enable : &mut Enable<P1,P2,P3,P4,P5>, steps : u32, direction : bool) 
+        where P1: PinOps, P2: PinOps, P3: PinOps, P4: PinOps, P5: PinOps{
             if direction {
                 self.dir.set_high();
             } else {
                 self.dir.set_low();
             }
+            if self.large {
+                enable.main.set_low();
+            } else {
+                enable.en4.set_low();
+                enable.en5.set_low();
+                enable.en6.set_low();
+            }
+            enable.led.set_high();
             for _x in 0..steps {
                 self.pulse.set_high();
                 hw::delay_us(self.delay);
                 self.pulse.set_low();
                 hw::delay_us(self.delay);
             }
+            if self.large {
+                enable.main.set_high();
+            } else {
+                enable.en4.set_high();
+                enable.en5.set_high();
+                enable.en6.set_high();
+            }
+            enable.led.set_low();
             true;
         }
     }
 
     pub struct Enable<P1,P2,P3,P4,P5> {
-        led  : Pin<Output, P1>,
-        main : Pin<Output, P2>,
-        en4  : Pin<Output, P3>,
-        en5  : Pin<Output, P4>,
-        en6  : Pin<Output, P5>
+        pub led  : Pin<Output, P1>,
+        pub main : Pin<Output, P2>,
+        pub en4  : Pin<Output, P3>,
+        pub en5  : Pin<Output, P4>,
+        pub en6  : Pin<Output, P5>
     }
-
-    pub fn make_enable (pins : Pins) -> Enable <PB7, PC5, PF2, PD7, PK0>{
-        Enable {
-            led  : pins.d13.into_output(),
-            main : pins.d32.into_output(),
-            en4  : pins.a2.into_output(),
-            en5  : pins.d38.into_output(),
-            en6  : pins.a8.into_output()
-        }
-    }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 mod axis;
 use axis::axis::make_axis;
-use axis::axis::make_enable;
+use axis::axis::Enable;
 
 use panic_halt as _;
 use arduino_hal as hw;
@@ -13,16 +13,13 @@ fn main() -> ! {
     let peripherals = hw::Peripherals::take().unwrap();
     let pins        = hw::pins!(peripherals);
 
-    let mut enable = make_enable(pins);
-
-    let mut led = pins.d13.into_output();
-    let mut main = pins.d32.into_output();
-    let mut enable_6 = pins.a8.into_output();
-    let mut enable_5 = pins.d38.into_output();
-    let mut enable_4 = pins.a2.into_output();
-    enable_4.set_high();
-    enable_5.set_high();
-    enable_6.set_high();
+    let mut enable = Enable {
+        led  : pins.d13.into_output(),
+        main : pins.d32.into_output(),
+        en4  : pins.a2 .into_output(),
+        en5  : pins.d38.into_output(),
+        en6  : pins.a8 .into_output()
+    };
 
     // Pulse, direction, delay
     let mut ax1 = make_axis(pins.d43, pins.d41, true, 4000);
@@ -32,35 +29,22 @@ fn main() -> ! {
     let mut ax5 = make_axis(pins.a0,  pins.a1,  false, 200);
     let mut ax6 = make_axis(pins.d46, pins.d48, false, 200);
 
-    main.set_low();
+    ax1.step(&mut enable, 500, true);
+    ax1.step(&mut enable, 500, false);
+    ax2.step(&mut enable, 1000, false);
+    ax2.step(&mut enable, 1000, true);
+    ax3.step(&mut enable, 1000, false);
+    ax3.step(&mut enable, 1000, true);
 
-    led.set_high();
-    ax1.step(500, true);
-    ax1.step(500, false);
-    ax2.step(1000, false);
-    ax2.step(1000, true);
-    ax3.step(1000, false);
-    ax3.step(1000, true);
-
-    enable_6.set_low();
-    enable_5.set_low();
-    enable_4.set_low();
-
-    ax4.step(6000, true);
-    ax4.step(6000, false);
-    ax5.step(6000, true);
-    ax5.step(6000, false);
-    ax6.step(6000, true);
-    ax6.step(6000, false);
-    led.set_low();
-
-    enable_4.set_high();
-    enable_5.set_high();
-    enable_6.set_high();
-    main.set_high();
+    ax4.step(&mut enable, 6000, true);
+    ax4.step(&mut enable, 6000, false);
+    ax5.step(&mut enable, 6000, true);
+    ax5.step(&mut enable, 6000, false);
+    ax6.step(&mut enable, 6000, true);
+    ax6.step(&mut enable, 6000, false);
 
     loop {
-        led.toggle();
+        enable.led.toggle();
         hw::delay_ms(100);
     }
 }
